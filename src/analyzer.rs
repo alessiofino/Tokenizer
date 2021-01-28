429a36a5ddc8c335892657c0f0ad3aa10ae5389a
qpadev <fino.alessio@gmail.com>
2021-01-28 11:28:21 +0000

    The analyzer now owns the stopwords set, autoformatting with rustfmt

--- src/analyzer.rs
+++ src/analyzer.rs
@@ -4,11 +4,13 @@ use fst::Set;
 use once_cell::sync::Lazy;
 
 use crate::detection::is_cjk;
-use crate::normalizer::{Normalizer, IdentityNormalizer, DeunicodeNormalizer, LowercaseNormalizer};
-use crate::processors::{PreProcessor, IdentityPreProcessor, ProcessedText, ChineseTranslationPreProcessor};
+use crate::normalizer::{DeunicodeNormalizer, IdentityNormalizer, LowercaseNormalizer, Normalizer};
+use crate::processors::{
+    ChineseTranslationPreProcessor, IdentityPreProcessor, PreProcessor, ProcessedText,
+};
 use crate::token_classifier::TokenClassifier;
+use crate::tokenizer::{Jieba, LegacyMeilisearch, TokenStream, Tokenizer, UnicodeSegmenter};
 use crate::Token;
-use crate::tokenizer::{Jieba, UnicodeSegmenter, TokenStream, Tokenizer, LegacyMeilisearch};
 
 static DEFAULT_PIPELINE: Lazy<Pipeline> = Lazy::new(Pipeline::default);
 
@@ -97,48 +99,64 @@ make_script! {
     Thai
 }
 
-pub struct AnalyzerConfig<'a, A> {
+pub struct AnalyzerConfig<A> {
     /// language specialized tokenizer, this can be switched during
     /// document tokenization if the document contains several languages
     pub pipeline_map: HashMap<(Script, Language), Pipeline>,
-    pub stop_words: &'a Set<A>,
+    pub stop_words: Set<A>,
 }
 
-impl<'a, A> AnalyzerConfig<'a, A>
+impl<A> AnalyzerConfig<A>
 where
     A: AsRef<[u8]>,
 {
-    pub fn default_with_stopwords(stop_words: &'a Set<A>) -> Self {
+    pub fn default_with_stopwords(stop_words: Set<A>) -> Self {
         let mut pipeline_map: HashMap<(Script, Language), Pipeline> = HashMap::new();
 
         // Latin script specialized pipeline
-        let latin_normalizer: Vec<Box<dyn Normalizer>> = vec![Box::new(DeunicodeNormalizer::default()), Box::new(LowercaseNormalizer)];
-        pipeline_map.insert((Script::Latin, Language::Other), Pipeline::default()
-            .set_tokenizer(LegacyMeilisearch)
-            .set_normalizer(latin_normalizer));
+        let latin_normalizer: Vec<Box<dyn Normalizer>> = vec![
+            Box::new(DeunicodeNormalizer::default()),
+            Box::new(LowercaseNormalizer),
+        ];
+        pipeline_map.insert(
+            (Script::Latin, Language::Other),
+            Pipeline::default()
+                .set_tokenizer(LegacyMeilisearch)
+                .set_normalizer(latin_normalizer),
+        );
 
         // Chinese script specialized pipeline
-        let chinese_deunicoder = DeunicodeNormalizer::new(&|text: &str| text.chars().next().map_or(false, is_cjk));
-        let chinese_normalizer: Vec<Box<dyn Normalizer>> = vec![Box::new(chinese_deunicoder), Box::new(LowercaseNormalizer)];
-        pipeline_map.insert((Script::Mandarin, Language::Other), Pipeline::default()
-            .set_pre_processor(ChineseTranslationPreProcessor)
-            .set_tokenizer(Jieba::default())
-            .set_normalizer(chinese_normalizer));
+        let chinese_deunicoder =
+            DeunicodeNormalizer::new(&|text: &str| text.chars().next().map_or(false, is_cjk));
+        let chinese_normalizer: Vec<Box<dyn Normalizer>> =
+            vec![Box::new(chinese_deunicoder), Box::new(LowercaseNormalizer)];
+        pipeline_map.insert(
+            (Script::Mandarin, Language::Other),
+            Pipeline::default()
+                .set_pre_processor(ChineseTranslationPreProcessor)
+                .set_tokenizer(Jieba::default())
+                .set_normalizer(chinese_normalizer),
+        );
 
-        AnalyzerConfig { pipeline_map, stop_words }
+        AnalyzerConfig {
+            pipeline_map,
+            stop_words,
+        }
     }
 
-    pub fn new(pipeline_map: HashMap<(Script, Language), Pipeline>, stop_words: &'a Set<A>) -> Self {
-        Self { pipeline_map, stop_words }
+    pub fn new(pipeline_map: HashMap<(Script, Language), Pipeline>, stop_words: Set<A>) -> Self {
+        Self {
+            pipeline_map,
+            stop_words,
+        }
     }
 }
 
-pub struct Analyzer<'a, A> {
-    config: AnalyzerConfig<'a, A>,
+pub struct Analyzer<A> {
+    config: AnalyzerConfig<A>,
 }
 
-pub struct AnalyzedText<'a, A>
-{
+pub struct AnalyzedText<'a, A> {
     /// Processed text
     processed: ProcessedText<'a>,
     /// Pipeline used to proccess the text
@@ -149,32 +167,33 @@ pub struct AnalyzedText<'a, A>
 
 impl<'a, A> AnalyzedText<'a, A>
 where
-    A: AsRef<[u8]>
+    A: AsRef<[u8]>,
 {
     /// Returns a `TokenStream` for the Analyzed text.
     pub fn tokens(&'a self) -> TokenStream<'a> {
-        let stream = self.pipeline.tokenizer
+        let stream = self
+            .pipeline
+            .tokenizer
             .tokenize(&self.processed)
             .map(move |t| self.pipeline.normalizer.normalize(t))
             .map(move |t| self.classifier.classify(t));
         TokenStream {
-            inner: Box::new(stream)
+            inner: Box::new(stream),
         }
     }
 
     /// Attaches each token to its corresponding portion of the original text.
     pub fn reconstruct(&'a self) -> impl Iterator<Item = (&'a str, Token<'a>)> {
-        self.tokens().map(move |t| (&self.processed.original[t.byte_start..t.byte_end], t))
+        self.tokens()
+            .map(move |t| (&self.processed.original[t.byte_start..t.byte_end], t))
     }
 }
 
-impl<'a, A> Analyzer<'a, A> {
+impl<A> Analyzer<A> {
     /// create a new tokenizer detecting script
     /// and chose the specialized internal tokenizer
-    pub fn new(config: AnalyzerConfig<'a, A>) -> Self {
-        Self {
-            config,
-        }
+    pub fn new(config: AnalyzerConfig<A>) -> Self {
+        Self { config }
     }
 
     /// Builds an `AnalyzedText` instance with the correct analyzer pipeline, and pre-processes the
@@ -202,7 +221,7 @@ impl<'a, A> Analyzer<'a, A> {
         AnalyzedText {
             processed,
             pipeline,
-            classifier
+            classifier,
         }
     }
 
@@ -215,16 +234,24 @@ impl<'a, A> Analyzer<'a, A> {
     fn pipeline_from(&self, text: &str) -> &Pipeline {
         let script = self.detect_script(text);
         let language = self.detect_lang(text);
-        self.config.pipeline_map.get(&(script, language))
+        self.config
+            .pipeline_map
+            .get(&(script, language))
             .or_else(|| self.config.pipeline_map.get(&(script, Language::Other)))
-            .or_else(|| self.config.pipeline_map.get(&(Script::Other, Language::Other)))
+            .or_else(|| {
+                self.config
+                    .pipeline_map
+                    .get(&(Script::Other, Language::Other))
+            })
             .unwrap_or_else(|| &*DEFAULT_PIPELINE)
     }
 
     /// detect script with whatlang,
     /// if no script is detected, return Script::Other
     fn detect_script(&self, text: &str) -> Script {
-        whatlang::detect_script(text).map(Script::from).unwrap_or(Script::Other)
+        whatlang::detect_script(text)
+            .map(Script::from)
+            .unwrap_or(Script::Other)
     }
 
     /// detect lang (dummy)
@@ -232,7 +259,6 @@ impl<'a, A> Analyzer<'a, A> {
         Language::Other
     }
 }
-
 
 #[cfg(test)]
 mod test {
@@ -264,7 +290,33 @@ mod test {
         let analyzed: Vec<_> = analyzed.tokens().map(|token| token.word).collect();
         assert_eq!(
             analyzed,
-            ["人人", "生而自由", ",", "在", "尊严", "和", "权利", "上", "一律平等", "。", "他们", "赋有", "理性", "和", "良心", ",", "并", "应以", "兄弟", "关系", "的", "精神", "互相", "对待", "。"]
+            [
+                "人人",
+                "生而自由",
+                ",",
+                "在",
+                "尊严",
+                "和",
+                "权利",
+                "上",
+                "一律平等",
+                "。",
+                "他们",
+                "赋有",
+                "理性",
+                "和",
+                "良心",
+                ",",
+                "并",
+                "应以",
+                "兄弟",
+                "关系",
+                "的",
+                "精神",
+                "互相",
+                "对待",
+                "。"
+            ]
         );
     }
 
@@ -281,14 +333,43 @@ mod test {
 
         assert_eq!(
             analyzed,
-            ["人人", "生而自由", ",", "在", "尊严", "和", "权利", "上", "一律平等", "。", "他们", "赋有", "理性", "和", "良心", ",", "并", "应以", "兄弟", "关系", "的", "精神", "互相", "对待", "。"]
+            [
+                "人人",
+                "生而自由",
+                ",",
+                "在",
+                "尊严",
+                "和",
+                "权利",
+                "上",
+                "一律平等",
+                "。",
+                "他们",
+                "赋有",
+                "理性",
+                "和",
+                "良心",
+                ",",
+                "并",
+                "应以",
+                "兄弟",
+                "关系",
+                "的",
+                "精神",
+                "互相",
+                "对待",
+                "。"
+            ]
         );
     }
 
     #[test]
     fn test_simple_latin_with_lowercase_normalizer() {
         let mut pipeline_map: HashMap<(Script, Language), Pipeline> = HashMap::new();
-        pipeline_map.insert((Script::Latin, Language::Other), Pipeline::default().set_normalizer(LowercaseNormalizer));
+        pipeline_map.insert(
+            (Script::Latin, Language::Other),
+            Pipeline::default().set_normalizer(LowercaseNormalizer),
+        );
 
         let stop_words = Set::default();
         let analyzer = Analyzer::new(AnalyzerConfig::new(pipeline_map, &stop_words));
@@ -303,7 +384,10 @@ mod test {
         let analyzer = Analyzer::new(AnalyzerConfig::default_with_stopwords(&stop_words));
         let orig = "The quick (\"brown\") fox can't jump 32.3 feet, right? Brr, it's 29.3°F!";
         let tokens = analyzer.analyze(orig);
-        assert_eq!(orig, tokens.reconstruct().map(|(t, _)| t).collect::<String>());
+        assert_eq!(
+            orig,
+            tokens.reconstruct().map(|(t, _)| t).collect::<String>()
+        );
     }
 
     #[test]
@@ -312,7 +396,10 @@ mod test {
         let analyzer = Analyzer::new(AnalyzerConfig::default_with_stopwords(&stop_words));
         let orig = "人人生而自由﹐在尊严和权利上一律平等。他们赋有理性和良心﹐并应以兄弟关系的精神互相对待。";
         let tokens = analyzer.analyze(orig);
-        assert_eq!(orig, tokens.reconstruct().map(|(t, _)| t).collect::<String>());
+        assert_eq!(
+            orig,
+            tokens.reconstruct().map(|(t, _)| t).collect::<String>()
+        );
     }
 
     #[test]
@@ -321,6 +408,9 @@ mod test {
         let analyzer = Analyzer::new(AnalyzerConfig::default_with_stopwords(&stop_words));
         let traditional = "人人生而自由﹐在尊嚴和權利上一律平等。他們賦有理性和良心﹐並應以兄弟關係的精神互相對待。";
         let tokens = analyzer.analyze(traditional);
-        assert_eq!(traditional, tokens.reconstruct().map(|(t, _)| t).collect::<String>());
+        assert_eq!(
+            traditional,
+            tokens.reconstruct().map(|(t, _)| t).collect::<String>()
+        );
     }
 }
